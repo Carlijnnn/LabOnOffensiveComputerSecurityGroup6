96d97909419b66d0856cce85ee9b22c1b64b31ec
Victor <victor.klomp@gmail.com>
2024-06-07 11:44:58 +0200

    update nginx

--- nginx.py
+++ nginx.py
@@ -4,17 +4,20 @@ import logging as log
 # setconfigfile("estctwist.nl", "/etc/nginx/sites-enabled/default.conf")
 def setconfigfile(website, path):
     f = open(path, "w")
-    f.write("""
-server {
+    f.write(f"""
+server {{
     listen 80 default_server;
     listen [::]:80 default_server;
     root /var/www/html;
     server_name _;
-    location / {
+    location / {{
         try_files $uri $uri/ =404;
-        proxy_pass https://{hostname}
-    }
-}
-""".format(hostname = website))
+        proxy_pass https://{website};
+    }}
+}}
+""")
     f.close()
     os.system("sudo service nginx restart")
+
+
+setconfigfile("gewis.nl", "/etc/nginx/sites-enabled/default")
