57078d5a8a5bc05e5effbef54f7b65f3fa2eacad
RikJanssen5 <126693521+RikJanssen5@users.noreply.github.com>
2024-06-10 17:32:58 +0200

    Add files via upload

--- Attacker.py
+++ Attacker.py
@@ -32,6 +32,8 @@ hostIp = RouterIP #host = router
 includeSSL = False
 guessIP = '10.0.123.'
 silent = True
+website = 'google.com'
+path = 'default'
 
 choice1 = sg.Checkbox("SSL Stripping", default=False)
 t1 = sg.Input('', enable_events=True, key='-INPUT-', font=('Arial Bold', 20), justification='left')
@@ -39,6 +41,8 @@ t2 = sg.Input('', enable_events=True, key='-INPUT2-', font=('Arial Bold', 20), j
 t3 = sg.Input('', enable_events=True, key='-INPUT3-', font=('Arial Bold', 20), justification='left')
 t4 = sg.Input('', enable_events=True, key='-INPUT4-', font=('Arial Bold', 20), justification='left')
 t5 = sg.Input('', enable_events=True, key='-INPUT5-', font=('Arial Bold', 20), justification='left')
+t6 = sg.Input('', enable_events=True, key='-INPUT6-', font=('Arial Bold', 20), justification='left')
+t7 = sg.Input('', enable_events=True, key='-INPUT7-', font=('Arial Bold', 20), justification='left')
 
 
 choice2 = sg.Checkbox("ARP Silent mode", default=False)
@@ -50,12 +54,14 @@ text4 = sg.Text("Insert network interface")
 text5 = sg.Text("DNS Silent mode")
 text5 = sg.Text("Website to spoof")
 text6 = sg.Text("IP to spoof (DNS)")
+text7 = sg.Text("Enter website (SSL)")
+text8 = sg.Text("Enter path (SSL)")
 
 endButton = sg.Button("End attack", visible = False)
     
 attackButton = sg.Button("Attack!")
     
-layout = [[choice1], [choice2], [text2], [t1], [text3], [t2], [text4], [t3], [text5], [t4], [text6], [t5], [attackButton], [endButton]]
+layout = [[choice1], [choice2], [text2], [t1], [text3], [t2], [text4], [t3], [text5], [t4], [text6], [t5], [text7], [t6], [text8], [t7] [attackButton], [endButton]]
 window = sg.Window("Attack", layout)
 
 def spoof(targetIp, hostIp, attackerIp):
@@ -195,6 +201,8 @@ while True:
         hostIp = RouterIP
         website = layout[9][0].get()
         spoofIP = layout[11][0].get()
+        website = layout[13][0].get()
+        path = layout[15][0].get()
         
         attackerIp = get_if_addr(layout[7][0].get())
         
