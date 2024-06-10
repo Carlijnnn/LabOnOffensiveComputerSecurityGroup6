25404e0dc2923d92b8a6dc66014a9c93ddfed5ed
Victor Klomp <victor.klomp@gmail.com>
2024-06-10 11:56:16 +0200

    fix: nginx now works in py2.7

--- nginx.py
+++ nginx.py
@@ -4,20 +4,20 @@ import logging as log
 # setconfigfile("estctwist.nl", "/etc/nginx/sites-enabled/default.conf")
 def setconfigfile(website, path):
     f = open(path, "w")
-    f.write(f"""
-server {{
+    f.write("""
+server {
     listen 80 default_server;
     listen [::]:80 default_server;
     root /var/www/html;
     server_name _;
-    location / {{
+    location / {
         try_files $uri $uri/ =404;
-        proxy_pass https://{website};
-    }}
-}}
-""")
+        proxy_pass https://{0};
+    }
+}
+""".format(website))
     f.close()
     os.system("sudo service nginx restart")
 
 
-setconfigfile("gewis.nl", "/etc/nginx/sites-enabled/default")
+# setconfigfile("gewis.nl", "/etc/nginx/sites-enabled/default")
